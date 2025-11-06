bc44653f735bfecabb81dd860ab189fa76fd8081
Zinny <edmunddebby24@gmail.com>
2025-11-06 12:45:54 +0100

    Pulled the lates commit from the main

--- src/pages/onboarding/Signup.tsx
+++ src/pages/onboarding/Signup.tsx
@@ -18,15 +18,15 @@ export default function Signup() {
         </div>
 
         {/* Right side */}
-        <div className="w-full md:w-1/2 flex flex-col justify-between p-8 min-h-[600px]">
-          <div className="text-center mb-6">
-            <h2 className="text-2xl font-semibold pb-2">Less Stress</h2>
-            <p className="text-2xl font-semibold">More Business</p>
+        <div className="w-full md:w-1/2 flex flex-col justify-between p-8 min-h-[550px]">
+          <div className="text-center mb-3 md:mt-7">
+            <h2 className="text-2xl font-medium pb-2">Less Stress</h2>
+            <p className="text-2xl font-medium">More Business</p>
           </div>
 
           <div className="w-full max-w-sm mx-auto space-y-6">
             <div>
-              <label className="block text-sm font-medium mb-1">
+              <label className="block text-lg md:text-sm font-medium md:mb-1 mb-2">
                 Phone Number
               </label>
               <input
@@ -34,17 +34,17 @@ export default function Signup() {
                 value={phoneNumber}
                 onChange={(e) => setPhoneNumber(e.target.value)}
                 placeholder="Put your phone number"
-                className="w-full border rounded-md p-2 outline-none"
+                className="w-full border rounded-md p-3 outline-none md:text-sm text-xl"
               />
             </div>
             <div>
-              <label className="block text-sm font-medium mb-1">Password</label>
+              <label className="block text-lg md:text-sm font-medium md:mb-1 mb-2">Password</label>
               <input
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Put your password"
-                className="w-full border rounded-md p-2 outline-none"
+                className="w-full border rounded-md p-3 outline-none md:text-sm text-xl"
               />
             </div>
           </div>
