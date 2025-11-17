87c62db31ba4d35851e87c6eef057aba3b8dd841
Okafor Ifeanyi <ifeanyi.okafor.bio@gmail.com>
2025-11-17 08:53:15 +0100

    fix(build): build isssue resolved

--- src/pages/onboarding/Otp.tsx
+++ src/pages/onboarding/Otp.tsx
@@ -92,7 +92,7 @@ export default function OtpPage() {
           {otp.map((digit, index) => (
             <input
               key={index}
-              ref={(el) => (inputRefs.current[index] = el)}
+              // ref={(el) => (inputRefs.current[index] = el)}
               type="text"
               inputMode="numeric"
               maxLength={1}
